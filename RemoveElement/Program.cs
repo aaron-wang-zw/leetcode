b6bfa1a8d70749618e11579be0a3c8d5fefbf2d4
AaronDW <30791514+AaronDW@users.noreply.github.com>
2019-11-02 23:39:00 +0800

    add three array function

---
+++ RemoveElement/Program.cs
@@ -0,0 +1,47 @@
+﻿// Source : https://leetcode-cn.com/problems/remove-element/
+// Author : aaron wang
+// Date   : 2019-11-02
+
+/**********************************************************************************
+ *
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ * 给定 nums = [3,2,2,3], val = 3
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 
+ **********************************************************************************/
+
+using System;
+
+namespace RemoveElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// removeElement
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="val"></param>
+        public static int RemoveElement(int[] nums, int val)
+        {
+            var i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
---
+++ TwoSum/Program.cs
@@ -0,0 +1,48 @@
+﻿// Source : https://leetcode-cn.com/problems/two-sum/
+// Author : aaron wang
+// Date   : 2019-11-02
+
+/**********************************************************************************
+ *
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例 1:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * 
+ **********************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            Console.WriteLine(TwoSum(nums, 9));
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// two sum
+        /// </summary>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] { i, dict[target - nums[i]] };
+                }
+                dict[nums[i]] = i;
+            }
+            return null;
+        }
+    }
+}
